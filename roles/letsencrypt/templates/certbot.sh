84ad4c9d438533e09c1900c5564f17639c6b855a
ssaart <ssaart@ttu.ee>
2022-04-15 10:18:16 +0300

    lots of updates and improvements - removed bind for just hostname update - switched to cephadm - made stuff more dynamic - added initial ceph configuration with cephadm - certs

---
+++ roles/letsencrypt/templates/certbot.sh
@@ -0,0 +1,9 @@
+/usr/local/bin/certbot certonly \
+  --non-interactive \
+  --agree-tos \
+  --email {{ certbot_email }} \
+  --dns-cloudflare \
+  --dns-cloudflare-credentials cloudflare.ini \
+  --dns-cloudflare-propagation-seconds 300 \
+  -d "{{ domain_name }}" \
+  -d "www.{{ domain_name }}"
